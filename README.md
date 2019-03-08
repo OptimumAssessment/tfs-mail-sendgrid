c9f30bb343b44130830e02030fa0efc22c09071c
Jean-Pierre Broeders <github@freelyit.nl>
2019-03-08 15:45:19 +0100

    Add first version of the SendGrid build/release task

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# tfs-mail-sendgrid
-Custom build task for sending mails (including attachments) via SendGrid
+# SendGrid Email Build/Release task
+This build/release task can send emails via SendGrid **including attachments**.
+
+## Prerequisite
+You need to have a SendGrid Account created with an appropriate plan (they also have a free plan).
+After you have an account you need to create an API key which can be easily done via the web interface from SendGrid.
+This API key is needed in the build/release task.
+
+# Configure SendGrid Section
+The Sendgrid section contains the following fields:
+* SendGrid Key (Required): API Key given by SendGrid. You can generate an API Key from the SendGrid Portal.
+
+
+# Configure Mail Section
+The Mail section contains the following fields:
+* **To Address** (Required): To Addresses. Separated by commas (,).
+* **From Address** (Required): From Address.
+* **Mail Subject** (Required): The subject of the email.
+* **Html Body** (Required): The (html) body of the email.
+* **Add Attachment** (Optional): Should be true in case you need to add an attachment.
+* **Attachment** (Optional): Absolute path of the attachment.
---
+++ cag-sendgrid.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const tl = require('vso-task-lib');
+const fs = require('fs');
+const sendgrid = require('@sendgrid/mail');
+
+const sendgridkey = tl.getInput('sendgridkey', true);
+const to = tl.getInput('to', true);
+const from = tl.getInput('from', true);
+const subject = tl.getInput('subject', true);
+const htmlbody = tl.getInput('htmlbody', true);
+const addattachment = tl.getBoolInput('addattachment', true);
+const attachment = tl.getPathInput('attachment', false);
+
+console.log(
+`
+=============
+Mail Settings
+=============
+To Address: ${to}
+From Adddress: ${from}
+Subject: ${subject}
+Body: ${htmlbody}
+Attachment included? ${addattachment}
+Attachment: ${attachment}
+`
+);
+
+sendgrid.setApiKey(sendgridkey);
+
+let msg = {
+    to: to,
+    from: from,
+    subject: subject,
+    html: htmlbody
+};
+
+try {
+    if(addattachment) { 
+        tl.checkPath(attachment, 'attachment');
+        const fileBase64 = fs.readFileSync(attachment).toString('base64');
+
+        msg.attachments = 
+        [
+            {
+                content: fileBase64,
+                filename: path.basename(attachment),
+                contentId: path.basename(attachment, path.extname(attachment))
+            }
+        ]
+    }
+
+    sendgrid.send(msg);
+      
+} catch (error) {
+    console.log(error);    
+    tl.exit(1);
+}
